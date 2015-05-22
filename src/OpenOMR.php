e25044c12ca6f9b134f28493e0bdc61b89cbe772
batusa <emmersonsiqueira@gmail.com>
2015-05-22 16:20:52 -0300

    corrected typo in attribute names; matrix X and Y length can now be set by setters;

--- src/OpenOMR.php
+++ src/OpenOMR.php
@@ -9,19 +9,20 @@ class OpenOMR
 {
     const EDGE = 4;
     const DEBUG = 0;
-    const MATRIXROWS = 54;
-    const MATRIXCOLS = 38;
     const ERROR_MARGIN = 0.5;
 
     private $debugFolder;
 
+    private $matrixXLength;
+    private $matrixYLength;
+
     private $img;
     private $imgSizeW;
     private $imgSizeH;
-    private $imgCelSizeW;
-    private $imgCelSizeH;
-    private $imgCelCompareSizeW;
-    private $imgCelCompareSizeH;
+    private $imgCellSizeW;
+    private $imgCellSizeH;
+    private $imgCellCompareSizeW;
+    private $imgCellCompareSizeH;
 
     public function __construct($path)
     {
@@ -31,7 +32,20 @@ class OpenOMR
 
         $this->img = new Imagick();
         $this->img->readImage($path);
+    }
 
+    public function setMatrixYLength($length)
+    {
+        $this->matrixYLength = $length;
+    }
+
+    public function setMatrixXLength($length)
+    {
+        $this->matrixXLength = $length;
+    }
+
+    public function getMarksFromPaths($paths)
+    {
         if (self::DEBUG) {
             $this->debugFolder = getdate()[0] . '/';
             mkdir($this->debugFolder);
@@ -41,88 +55,20 @@ class OpenOMR
         if (self::DEBUG) {
             $this->img->writeImage($this->debugFolder . 'removeColors.PNG');
         }
+
         $this->adjustImage();
         if (self::DEBUG) {
             $this->img->writeImage($this->debugFolder . 'adjustImage.PNG');
         }
+
         $this->removeEdges();
         if (self::DEBUG) {
             $this->img->writeImage($this->debugFolder . 'removeEdges.PNG');
         }
-    }
 
-    private function removeColors()
-    {
-        $imginfo = $this->img->identifyImage();
-        if ($imginfo['type'] == 'TrueColor') {
-            // remove red from image
-            $this->img->separateImageChannel(Imagick::CHANNEL_RED);
-        } else {
-            // desaturate
-            $this->img->modulateImage(100, 0, 100);
-        }
-    }
-
-    private function adjustImage()
-    {
-        $this->img->normalizeImage(Imagick::CHANNEL_ALL);
-        $this->img->enhanceImage();
-        $this->img->despeckleImage();
-
-        $this->img->blackthresholdImage('#808080');
-        $this->img->whitethresholdImage('#808080');
-    }
-
-    private function removeEdges()
-    {
-
-        //remove edges and possible skew from image
-        $this->img->trimImage(85);
-        $this->img->deskewImage(15);
-        $this->img->trimImage(85);
-        $this->img->setImagePage(0, 0, 0, 0);
-
-        $this->calculateSizes();
-    }
-
-    private function calculateSizes()
-    {
-
-        $imginfo = $this->img->identifyImage();
-        $this->imgSizeW = $imginfo['geometry']['width'];
-        $this->imgSizeH = $imginfo['geometry']['height'];
-
-        $this->imgCelSizeW = $this->imgSizeW / self::MATRIXCOLS;
-        $this->imgCelSizeH = $this->imgSizeH / self::MATRIXROWS;
-
-        $this->imgCelCompareSizeW = $this->imgCelSizeW - (self::EDGE * 2);
-        $this->imgCelCompareSizeH = $this->imgCelSizeH - (self::EDGE * 2);
-
-        if (self::DEBUG) {
-            print_r($this);
-        }
-
-    }
-
-    private function getRegionFromImage($row, $col)
-    {
-        $sizeW = $this->imgCelCompareSizeW;
-        $sizeH = $this->imgCelCompareSizeH;
-        $pX = ($col * $this->imgCelSizeW) + self::EDGE;
-        $pY = ($row * $this->imgCelSizeW) + self::EDGE;
-
-        if (self::DEBUG) {
-            echo "\n" . $col . " - " . $row . " - " . $pX . " - " . $pY;
-        }
-
-        return $this->img->getImageRegion($sizeW, $sizeH, $pX, $pY);
-    }
-
-    public function getMarksFromPaths($paths)
-    {
         // create reference block pattern
         $imageToCompare = new Imagick();
-        $imageToCompare->newImage($this->imgCelCompareSizeW, $this->imgCelCompareSizeH, new ImagickPixel('black'));
+        $imageToCompare->newImage($this->imgCellCompareSizeW, $this->imgCellCompareSizeH, new ImagickPixel('black'));
 
         $result = [];
 
@@ -182,6 +128,72 @@ class OpenOMR
         $imageToCompare->clear();
 
         return $result;
+    }
+
+    private function identifyImage()
+    {
+        return $this->img->identifyImage();
+    }
+
+    private function removeColors()
+    {
+        if ($this->identifyImage()['type'] === 'TrueColor') {
+            $this->img->separateImageChannel(Imagick::CHANNEL_RED); // remove red from image
+        } else {
+            $this->img->modulateImage(100, 0, 100); // desaturate
+        }
+    }
+
+    private function adjustImage()
+    {
+        $this->img->normalizeImage(Imagick::CHANNEL_ALL);
+        $this->img->enhanceImage();
+        $this->img->despeckleImage();
+        $this->img->blackthresholdImage('#808080');
+        $this->img->whitethresholdImage('#808080');
+    }
+
+    private function removeEdges()
+    {
+        //remove edges and possible skew from image
+        $this->img->trimImage(85);
+        $this->img->deskewImage(15);
+        $this->img->trimImage(85);
+        $this->img->setImagePage(0, 0, 0, 0);
+
+        $this->calculateSizes();
+    }
+
+    private function calculateSizes()
+    {
+        $imageGeometryInfo = $this->identifyImage()['geometry'];
+        $this->imgSizeW = $imageGeometryInfo['width'];
+        $this->imgSizeH = $imageGeometryInfo['height'];
+
+        $this->imgCellSizeW = $this->imgSizeW / $this->matrixXLength;
+        $this->imgCellSizeH = $this->imgSizeH / $this->matrixYLength;
+
+        $this->imgCellCompareSizeW = $this->imgCellSizeW - (self::EDGE * 2);
+        $this->imgCellCompareSizeH = $this->imgCellSizeH - (self::EDGE * 2);
+
+        if (self::DEBUG) {
+            print_r($this);
+        }
+
+    }
+
+    private function getRegionFromImage($row, $col)
+    {
+        $sizeW = $this->imgCellCompareSizeW;
+        $sizeH = $this->imgCellCompareSizeH;
+        $pX = ($col * $this->imgCellSizeW) + self::EDGE;
+        $pY = ($row * $this->imgCellSizeW) + self::EDGE;
+
+        if (self::DEBUG) {
+            echo "\n" . $col . " - " . $row . " - " . $pX . " - " . $pY;
+        }
+
+        return $this->img->getImageRegion($sizeW, $sizeH, $pX, $pY);
     }
 
 }
