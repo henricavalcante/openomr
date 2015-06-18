d80fba1ecde95dad0b7caced23c73feb9efdb6a1
Emmerson <emmersonsiqueira@gmail.com>
2015-06-18 15:44:28 -0300

    image file is not passed in PaperSheet class anymore

--- src/PaperSheet.php
+++ src/PaperSheet.php
@@ -1,22 +1,14 @@
 <?php
 namespace OpenOMR;
 
-use OpenOMR\PaperSheetInterface;
-use OpenOMR\Field;
-
 class PaperSheet implements PaperSheetInterface
 {
-    private $source;
     private $matrixLength = [];
     private $fields = [];
 
-    public function __construct($source, array $matrixLength = null)
+    public function __construct(array $matrixLength)
     {
-        $this->source = $source;
-
-        if ($matrixLength) {
-            $this->setMatrixLength($matrixLength[0], $matrixLength[1]);
-        }
+        $this->setMatrixLength($matrixLength[0], $matrixLength[1]);
     }
 
     public function setMatrixLength($x, $y)
@@ -27,11 +19,6 @@ class PaperSheet implements PaperSheetInterface
     public function setField(Field $field)
     {
         $this->fields[] = $field;
-    }
-
-    public function getSource()
-    {
-        return $this->source;
     }
 
     public function getMatrixLength()
--- src/PaperSheetInterface.php
+++ src/PaperSheetInterface.php
@@ -1,14 +1,11 @@
 <?php
 namespace OpenOMR;
 
-use OpenOMR\Field;
-
 interface PaperSheetInterface
 {
-    public function __construct($source, array $matrixLength = null);
+    public function __construct(array $matrixLength);
     public function setMatrixLength($x, $y);
     public function setField(Field $field);
-    public function getSource();
     public function getMatrixLength();
     public function getFields();
 }
